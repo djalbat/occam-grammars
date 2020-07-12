dfc6cbc34d5a8c7770e631d591622d7868545e0f
James Smith <james.smith@openmathematics.org>
2020-07-12 10:15:37 +0100

    Cosmetic changes to the index file.

--- es6/index.js
+++ es6/index.js
@@ -1,17 +1,15 @@
 "use strict";
 
 export { default as PlainLexer } from "./plain/lexer";
-export { default as LaTeXLexer } from "./laTeX/lexer";
-export { default as MetaJSONLexer } from "./metaJSON/lexer";
-export { default as FlorenceLexer } from "./florence/lexer";
-export { default as CustomGrammarBNFLexer } from "./customGrammarBNF/lexer";
-export { default as CustomGrammarLexicalPatternLexer } from "./customGrammarLexicalPattern/lexer";
-
 export { default as PlainParser } from "./plain/parser";
+export { default as LaTeXLexer } from "./laTeX/lexer";
 export { default as LaTeXParser } from "./laTeX/parser";
+export { default as MetaJSONLexer } from "./metaJSON/lexer";
 export { default as MetaJSONParser } from "./metaJSON/parser";
+export { default as FlorenceLexer } from "./florence/lexer";
 export { default as FlorenceParser } from "./florence/parser";
+export { default as CustomGrammarBNFLexer } from "./customGrammarBNF/lexer";
 export { default as CustomGrammarBNFParser } from "./customGrammarBNF/parser";
+export { default as CustomGrammarLexicalPatternLexer } from "./customGrammarLexicalPattern/lexer";
 export { default as CustomGrammarLexicalPatternParser } from "./customGrammarLexicalPattern/parser";
-
 export { default as defaultCustomGrammar } from "./florence/defaultCustomGrammar";
