e5a69f0fe73caf0c2c902e73ba7434ed03d7e336
James Smith <james.smith@djalbat.com>
2021-06-02 19:52:03 +0100

    Removed the Statement and Context tokens from the Florence grammar.

--- src/florence/entries.js
+++ src/florence/entries.js
@@ -5,7 +5,7 @@ const entries = [
     "special": "^(?:,|=|::|:|\\|-|\\(|\\))|\\[|\\]|\\.\\.\\."
   },
   {
-    "keyword": "^(?:Rule|Axiom|Theorem|Lemma|Conjecture|Metalemma|Metatheorem|Premises|Premise|Conclusion|Proof|Therefore|Suppose|Then|Hence|Types|Type|Operators|Operator|Variables|Variable|Constructors|Constructor|DependentTypes|DependentType|DisjointTypes|DisjointType|Metavariables|Metavariable|Abbreviations|Abbreviation|Statement|Context|Let|for|from|by)\\b"
+    "keyword": "^(?:Rule|Axiom|Theorem|Lemma|Conjecture|Metalemma|Metatheorem|Premises|Premise|Conclusion|Proof|Therefore|Suppose|Then|Hence|Types|Type|Operators|Operator|Variables|Variable|Constructors|Constructor|DependentTypes|DependentType|DisjointTypes|DisjointType|Metavariables|Metavariable|Abbreviations|Abbreviation|Let|for|from|by)\\b"
   },
   {
     "reserved": "^(?:undefined|free|not|in|is)\\b"
