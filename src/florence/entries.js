b56ab6db867833845ce6943687eeb962cfeab6d1
James Smith <james.smith@djalbat.com>
2023-02-11 15:06:41 +0000

    Propagated.

--- src/florence/entries.js
+++ src/florence/entries.js
@@ -8,7 +8,7 @@ const entries = [
     "keyword": "^(?:Rule|Axiom|Theorem|Lemma|Conjecture|Metalemma|Metatheorem|Premises|Premise|Conclusion|Proof|Therefore|Suppose|Hence|Then|Type|Variable|Constructor|Combinator|DependentType|DisjointType|Metavariable|from|by|undefined|free|not|in|is)\\b"
   },
   {
-    "meta-type": "^(?:Statement)\\b"
+    "meta-type": "^(?:Statement|Context)\\b"
   },
   {
     "name": "^[A-Za-zΑ-Ωα-ω_0-9]+"
