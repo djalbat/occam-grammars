4f1874ea4cca8088d50ed5eafad89c84d81b56ee
James Smith <james.smith@djalbat.com>
2023-02-16 18:26:55 +0000

    Slight changes to the Florence entries.

--- src/florence/entries.js
+++ src/florence/entries.js
@@ -5,7 +5,7 @@ const entries = [
     "special": "^(?:,|::|:|\\|-|\\|=|\\(|\\)|\\[|\\]|\\.\\.\\.)"
   },
   {
-    "keyword": "^(?:Rule|Axiom|Theorem|Lemma|Conjecture|Metalemma|Metatheorem|Premises|Premise|Conclusion|Proof|Therefore|Suppose|Hence|Then|Type|Variable|Constructor|Combinator|DependentType|DisjointType|Metavariable|from|by|undefined|free|not|in|is)\\b"
+    "keyword": "^(?:Rule|Axiom|Theorem|Lemma|Conjecture|Metalemma|Metatheorem|Premises|Premise|Conclusion|Proof|Therefore|Suppose|Hence|Then|Type|Variable|Constructor|Combinator|DependentType|DisjointType|Metavariable|from|by|for|undefined)\\b"
   },
   {
     "meta-type": "^(?:Statement|Context)\\b"
