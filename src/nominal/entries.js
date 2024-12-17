59182be34286c1412b3de981643b32ea3ddcead3
James Smith <james.smith@djalbat.com>
2024-12-17 09:07:14 +0000

    And again.

--- src/nominal/entries.js
+++ src/nominal/entries.js
@@ -11,7 +11,7 @@ const entries = [
     "primary-keyword": "^(?:Rule|Axiom|Theorem|Lemma|Conjecture|MetaLemma|Metatheorem|Premises|Premise|Conclusion|Proof|Therefore|Suppose|Hence|Then|Type|Properties|Property|Variable|Constructor|Combinator|DependentType|DisjointType|Metavariable)\\b"
   },
   {
-    "secondary-keyword": "^(?:is|in|an|a|of|by|from|for|not)\\b"
+    "secondary-keyword": "^(?:is|in|the|an|a|of|by|from|for|not)\\b"
   },
   {
     "meta-type": "^(?:Statement|Reference|Frame)\\b"
