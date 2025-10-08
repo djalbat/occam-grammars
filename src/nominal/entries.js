21da0ab39e097384362e31d3c7948ed989998c45
James Smith <james.smith@djalbat.com>
2025-10-08 20:01:32 +0100

    Generalised the identifier expression.

--- src/nominal/entries.js
+++ src/nominal/entries.js
@@ -1,5 +1,12 @@
 "use strict";
 
+const LATIN = "[\\p{Script=Latin}]",
+      GREEK = "[\\p{Script=Greek}]",
+      SCRIPT = "[\\u{1D49C}-\\u{1D4CF}\\u{210A}\\u{210B}\\u{2110}\\u{2112}\\u{211B}\\u{212C}\\u{2130}\\u{2131}\\u{2133}\\u{2134}]",
+      DECIMAL = "[0-9]",
+      FRAKTUR = "[\\u{1D504}-\\u{1D537}\\u{210C}\\u{2111}\\u{211C}\\u{2128}\\u{212D}]",
+      SANS_SERIF = "[\\u{1D5D4}-\\u{1D607}]";
+
 const entries = [
   {
     "primary-keyword": "^(?:Rule|Axiom|Theorem|Lemma|Conjecture|MetaLemma|Metatheorem|Premises|Premise|Conclusion|Proof|Therefore|Suppose|Hence|Then|Provisional|Type|Properties|Property|Variable|Constructor|Combinator|Metavariable|Given)\\b"
@@ -14,7 +21,7 @@ const entries = [
     "name": "^(?:[a-z][A-Za-z]+[0-9]*|[A-Z][A-Za-z]*[0-9]*)"
   },
   {
-    "identifier": "^(?:[\\p{Script=Latin}]+[0-9]*|[\\p{Script=Greek}]+|[\\u{1D5D4}-\\u{1D607}]|[\\u{1D504}-\\u{1D537}\\u{210C}\\u{2111}\\u{211C}\\u{2128}\\u{212D}]|[\\u{1D49C}-\\u{1D4CF}\\u{210A}\\u{210B}\\u{2110}\\u{2112}\\u{211B}\\u{212C}\\u{2130}\\u{2131}\\u{2133}\\u{2134}])"
+    "identifier": `^(?:${LATIN}+${DECIMAL}*|${GREEK}+|${SANS_SERIF}+|${FRAKTUR}+|${SCRIPT}+)`
   },
   {
     "delimiter": "^(?:\\.\\.\\.|,|\\(|\\)|\\[|\\])"
