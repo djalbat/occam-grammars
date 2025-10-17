9213b06f20ce2024f33d632ee4feb69239ab24a7
James Smith <james.smith@djalbat.com>
2025-10-17 19:18:40 +0100

    Propagated.

--- src/nominal/entries.js
+++ src/nominal/entries.js
@@ -3,11 +3,12 @@
 const DECIMAL = "[0-9]",
       GREEK = "[\\p{Script=Greek}]",
       LATIN = "[\\p{Script=Latin}]",
-      LOWER_CASE_LATIN = "[\\p{Script=Latin}&&\\p{Lowercase}]",
-      UPPER_CASE_LATIN = "[\\p{Script=Latin}&&\\p{Uppercase}]",
-      MATHEMATICAL_SCRIPT = "[\\u{1D49C}\\u{212C}\\u{1D49E}\\u{1D49F}\\u{2130}\\u{2131}\\u{1D4A2}\\u{210B}\\u{2110}\\u{1D4A5}\\u{1D4A6}\\u{2112}\\u{2133}\\u{1D4A9}-\\u{1D4AC}\\u{211B}\\u{1D4AE}-\\u{1D4B5}\\u{1D4B6}-\\u{1D4CF}]",
-      MATHEMATICAL_FRAKTUR = "[\\u{1D504}\\u{1D505}\\u{212D}\\u{1D507}-\\u{1D50A}\\u{210C}\\u{2111}\\u{1D50D}-\\u{1D514}\\u{211C}\\u{1D516}-\\u{1D51C}\\u{2128}\\u{1D51E}-\\u{1D537}]",
-      MATHEMATICAL_SANS_SERIF_BOLD = "[\\u{1D5D4}-\\u{1D607}]";
+      LATIN_LOWER_CASE = "[\\p{Script=Latin}&&\\p{Lowercase}]",
+      LATIN_UPPER_CASE = "[\\p{Script=Latin}&&\\p{Uppercase}]",
+      MATHEMATICAL_SANS_SERIF_BOLD = "[\\u{1D5D4}-\\u{1D607}]",
+      MATHEMATICAL_ITALIC_UPPER_CASE = "[\\u{1D434}-\\u{1D44D}]",
+      MATHEMATICAL_SCRIPT_UPPER_CASE = "[\\u{1D49C}\\u{212C}\\u{1D49E}\\u{1D49F}\\u{2130}\\u{2131}\\u{1D4A2}\\u{210B}\\u{2110}\\u{1D4A5}\\u{1D4A6}\\u{2112}\\u{2133}\\u{1D4A9}-\\u{1D4AC}\\u{211B}\\u{1D4AE}-\\u{1D4B5}]",
+      MATHEMATICAL_FRAKTUR_UPPER_CASE = "[\\u{1D504}\\u{1D505}\\u{212D}\\u{1D507}-\\u{1D50A}\\u{210C}\\u{2111}\\u{1D50D}-\\u{1D514}\\u{211C}\\u{1D516}-\\u{1D51C}\\u{2128}]";
 
 const entries = [
   {
@@ -20,10 +21,10 @@ const entries = [
     "meta-type": "^(?:Statement|Reference|Frame)\\b"
   },
   {
-    "name": `^(?:${LOWER_CASE_LATIN}${LATIN}+${DECIMAL}*|${UPPER_CASE_LATIN}${LATIN}*${DECIMAL}*)`,
+    "name": `^${LATIN_UPPER_CASE}${LATIN}*${DECIMAL}*`,
   },
   {
-    "identifier": `^(?:${LATIN}+${DECIMAL}*|${GREEK}+|${MATHEMATICAL_SCRIPT}+|${MATHEMATICAL_FRAKTUR}+|${MATHEMATICAL_SANS_SERIF_BOLD}+)`
+    "identifier": `^(?:${LATIN_LOWER_CASE}+${DECIMAL}*|${GREEK}+|${MATHEMATICAL_SANS_SERIF_BOLD}+|${MATHEMATICAL_ITALIC_UPPER_CASE}+|${MATHEMATICAL_SCRIPT_UPPER_CASE}+|${MATHEMATICAL_FRAKTUR_UPPER_CASE}+)`
   },
   {
     "delimiter": "^(?:\\.\\.\\.|,|\\(|\\)|\\[|\\])"
