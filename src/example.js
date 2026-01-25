ab0f1e88ec4802a9d8b6dab8246967f2bbf6495a
James Smith <james.smith@djalbat.com>
2026-01-25 18:01:28 +0000

    Gotten rid of the FUrtle functionality. Exported just the lexer and parser.

--- src/example.js
+++ src/example.js
@@ -8,7 +8,6 @@ import { Body } from "easy";
 
 import TeXView from "./example/view/tex";
 import JSONView from "./example/view/json";
-import FurtleView from "./example/view/furtle";
 import PlainTextView from "./example/view/plainText";
 import CustomGrammarBNFView from "./example/view/customGrammarBNF";
 import CustomGrammarVocabularyView from "./example/view/customGrammarVocabulary";
@@ -23,7 +22,6 @@ const body = new Body(),
 switch (example) {
   case "tex" : View = TeXView; break;
   case "json" : View = JSONView; break;
-  case "furtle" : View = FurtleView; break;
   case "plain-text" : View = PlainTextView; break;
   case "custom-grammar-bnf" : View = CustomGrammarBNFView; break;
   case "custom-grammar-vocabulary" : View = CustomGrammarVocabularyView; break;
