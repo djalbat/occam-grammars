ad581246a523e758d3a75cca03a5f2e9ab6ab757
James Smith <james.smith@djalbat.com>
2026-09-18 22:05:18 +0100

    Added isolated parts to the custom grammar BNF.

--- src/customGrammarBNF/bnf.js
+++ src/customGrammarBNF/bnf.js
@@ -18,7 +18,9 @@ const bnf = `
 
                                  ;
 
-      nonTerminalPart          ::=  choiceOfParts
+      nonTerminalPart          ::=  isolatedPart
+
+                                 |  choiceOfParts
 
                                  |  sequenceOfParts
 
@@ -43,6 +45,8 @@ const bnf = `
       sequenceOfParts          ::=  "(" part part+ ")" ;
 
       choiceOfParts            ::=  "(" partChoice ( "|" partChoice )+ ")" ;
+
+      isolatedPart             ::=  "(" part ")" ;
 
       partChoice               ::=  part precedence? ;
 
